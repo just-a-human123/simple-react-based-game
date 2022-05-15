fdf451857d05c378ee1d0b24aea3a314c09bd9ac
alfazari <faa04807@gmail.com>
2022-05-16 01:19:12 +0700

    idk what i do

---
+++ src/components/snake-ladder/description.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Description = () => {
+  return (
+    <>
+      <h2>Snake and Ladder</h2>
+      <p>classic game of snake and ladder.</p>
+    </>
+  );
+};
+
+export default Description;
