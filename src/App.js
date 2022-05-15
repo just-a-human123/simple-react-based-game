35c02a255178ae5018f1bd668663483ca23ebdbc
alfazari <faa04807@gmail.com>
2022-05-15 23:47:30 +0700

    try&add: dice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import GameBoard from "./components/snake-ladder/gameboard";
+import Dice from "./components/snake-ladder/dice";
 
 function App() {
   return (
     <div className="App">
       <GameBoard/>
+      <Dice/>
     </div>
   );
 }
